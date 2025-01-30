1c423f4042a84333428526f1115ef2c15dd5ccb7
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-01-30 04:54:52 +0000

    created rock erb file and instance variables

--- app.rb
+++ app.rb
@@ -7,3 +7,18 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  elsif @comp_move == "scissors"
+    @outcome = "won"
+  end
+
+erb(:giraffe)
+end 
